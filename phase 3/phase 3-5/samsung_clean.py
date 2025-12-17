b2a80d07f3a4bc421394c19ee11456f5563eca9c
pilmalion114 <pilmalion114@naver.com>
2025-12-17 15:46:55 +0900

    python 파일 주석 추가

--- phase 3/phase 3-5/samsung_clean.py
+++ phase 3/phase 3-5/samsung_clean.py
@@ -2,8 +2,8 @@
 # BOM Encoding 문제로 인해 Glue Crawler가 CSV 헤더를 제대로 인식하지 못해 Athena 쿼리가 실패.
 # 해결: encoding='utf-8'로 설정하여 BOM 없는 새로운 CSV 파일 생성 후 재업로드.
 
-# 아.. 우리가 전에 'utf-8-sig'로 저장해서 BOM이 생겼나봄.. -> 이것도 내가 따로 첨부한 png 사진 참고하기!
-# 내가 따로 첨부한 'utf-8' vs 'utf-8-sig'.png 참고하기!
+# 아.. 우리가 전에 'utf-8-sig'로 저장해서 BOM이 생겼나봄.. -> 이것도 내가 따로 첨부한 png 사진 참고하기! -> 해당 사진에는 윗 부분이 좀 잘렸는데, 'utf-8-sig'로 작성한 부분은 Phase 2 또는 Phase 3 초반에 있다고 함. 한번 찾아보면 됨.
+# 내가 따로 첨부한 'utf-8' vs 'utf-8-sig'.png 참고하기! 
 
 import pandas as pd
 import FinanceDataReader as fdr
@@ -21,4 +21,3 @@ with open('./samsung_clean.csv','r',encoding='utf-8') as f:
     first_line = f.readline() # readline(): 당연히 유추 가능하겠지만, '파일에서 한 줄씩 읽기'를 실행하는 코드.
     print(repr(first_line)) # repr(): representation의 줄임말. 즉, '표현' 보여주기(일반 print()보다 더 세세하게 보여준다고 이해하면 됨.). repr()을 사용함으로써 숨겨진 문자를 확인할 수 있다.(예를 들어 공백 문자나 줄 바꿈 문자 같은 일반 print에는 직접적으로 안 보이는 문자들). repr()을 쓰면 만약 BOM이 있다면, '\ufeffDate,Open' 이렇게 나온다고 함.
 
-    
